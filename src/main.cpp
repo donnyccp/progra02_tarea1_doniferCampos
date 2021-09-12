47a9414ad439e747d00ca7c9b6e8341e0ed5cce2
donnyccp <onifer.campos@gmail.com>
2021-09-12 09:04:31 -0600

    Actalizacion

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,14 @@ using namespace std;
 
 int main(){
 
+    FedEx *FedEx1=new FedEx(500.0, 4.0);
+    FedEx *FedEx2=new FedEx(600.0, 5.0);
+    FedEx *FedEx3=new FedEx(700.0, 3.0);
+    FedEx *FedEx4=new FedEx(400.0, 3.0);
+
+    
+
+    
     double totalMontoEnvios = 0;
         for (Envio* envio : arregloEnvios)
         {
