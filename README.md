ca8c8675df0bb5fe62985602eeff51adf261dfcf
donnyccp <onifer.campos@gmail.com>
2021-09-12 13:05:01 -0600

    Complementacion al readme, version casi final

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # progra02_tarea1_doniferCampos
 Programación 02- Tarea 1-Donifer Campos Parra  <br />
+Programado en Windows10- Visual Studio Code 1.60.0 <br />
 mkdir obj <br />
 g++ -std=c++17 -stdlib=libc++ -g ./src/*.cpp -o ./obj/tarea1  <br />
 
--- src/main.cpp
+++ src/main.cpp
@@ -12,18 +12,16 @@ int main()
 {
 
     FedEx *FedEx1 = new FedEx(500.0, 4.0);
-    //FedEx *FedEx2 = new FedEx(600.0, 5.0);
-    //FedEx *FedEx3 = new FedEx(700.0, 3.0);
-    FedEx *FedEx4 = new FedEx(400.0, 3.0);
+    FedEx *FedEx2 = new FedEx(600.0, 6.0);
 
-    //ServicioPostal *servicioPostal1 = new ServicioPostal(2.0, 10.0, 1);
-    //ServicioPostal *servicioPostal2 = new ServicioPostal(5.0, 265.0, 2);
-    ServicioPostal *servicioPostal3 = new ServicioPostal(10.0, 100.0, 3);
+    ServicioPostal *servicioPostal1 = new ServicioPostal(5.0, 265.0, 2);
+    ServicioPostal *servicioPostal2 = new ServicioPostal(10.0, 100.0, 3);
 
     std::vector<Envio *> arregloEnvios;
     arregloEnvios.push_back(FedEx1);
-    arregloEnvios.push_back(FedEx4);
-    arregloEnvios.push_back(servicioPostal3);
+    arregloEnvios.push_back(FedEx2);
+    arregloEnvios.push_back(servicioPostal1);
+    arregloEnvios.push_back(servicioPostal2);
 
     double totalMontoEnvios = 0;
     for (Envio *envio : arregloEnvios)
@@ -33,16 +31,10 @@ int main()
     std::cout << "El monto total de los envíos es de " << totalMontoEnvios << std::endl;
 
     delete FedEx1;
-    //delete FedEx2;
-    //delete FedEx3;
-    delete FedEx4;
+    delete FedEx2;
 
-    //delete servicioPostal1;
-    //delete servicioPostal2;
-    delete servicioPostal3;
-
-  
-
+    delete servicioPostal1;
+    delete servicioPostal2;
 
     return 0;
 }
