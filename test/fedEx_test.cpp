f8211a41d2450373ac81803f4b207c573b7792d3
donnyccp <onifer.campos@gmail.com>
2021-09-12 17:57:37 -0600

    Entrega Final-Aplicacion de los tests

--- test/fedEx_test.cpp
+++ test/fedEx_test.cpp
@@ -3,11 +3,32 @@
 
 namespace
 {
-    TEST(FedExTests, CalculoEnvio_Test)
+    TEST(FedExTests, CalculoEnvio_sinRecargo_Test)
+    {
+        FedEx fedEx{400.0, 4.0};
+        float actual = fedEx.CalculoEnvio();
+        EXPECT_FLOAT_EQ(20.0, actual);
+    }
+
+     TEST(FedExTests, CalculoEnvio_conRecargo_peso_Test)
+    {
+        FedEx fedEx{400.0, 6.0};
+        float actual = fedEx.CalculoEnvio();
+        EXPECT_FLOAT_EQ(23.0, actual);
+    }
+
+     TEST(FedExTests, CalculoEnvio_conRecargo_pesoDistancia_Test)
     {
         FedEx fedEx{600.0, 6.0};
         float actual = fedEx.CalculoEnvio();
         EXPECT_FLOAT_EQ(28.0, actual);
+    }
+
+     TEST(FedExTests, CalculoEnvio_conRecargo_distacia_Test)
+    {
+        FedEx fedEx{600.0, 3.0};
+        float actual = fedEx.CalculoEnvio();
+        EXPECT_FLOAT_EQ(25.0, actual);
     }
 
 }
--- test/servicioPostal_test.cpp
+++ test/servicioPostal_test.cpp
@@ -3,10 +3,66 @@
 
 namespace
 {
-    TEST(ServicioPostalTests, CalculoEnvio_Test)
+    TEST(ServicioPostalTests, CalculoEnvio_primeraClase_situacion1_Test)
+    {
+        ServicioPostal servicioPostal{2.0, 2.0, 1};
+        float actual = servicioPostal.CalculoEnvio();
+        EXPECT_FLOAT_EQ(0.390, actual);
+    }
+
+     TEST(ServicioPostalTests, CalculoEnvio_primeraClase_situacion2_Test)
     {
         ServicioPostal servicioPostal{4.0, 2.0, 1};
         float actual = servicioPostal.CalculoEnvio();
         EXPECT_FLOAT_EQ(0.900, actual);
+    }
+
+     TEST(ServicioPostalTests, CalculoEnvio_primeraClase_situacion3_Test)
+    {
+        ServicioPostal servicioPostal{10.0, 2.0, 1};
+        float actual = servicioPostal.CalculoEnvio();
+        EXPECT_FLOAT_EQ(1.000, actual);
+    }
+
+     TEST(ServicioPostalTests, CalculoEnvio_segundaClase_situacion1_Test)
+    {
+        ServicioPostal servicioPostal{1.0, 3.0, 2};
+        float actual = servicioPostal.CalculoEnvio();
+        EXPECT_FLOAT_EQ(0.0585, actual);
+    }
+
+     TEST(ServicioPostalTests, CalculoEnvio_segundaClase_situacion2_Test)
+    {
+        ServicioPostal servicioPostal{5.0, 3.0, 2};
+        float actual = servicioPostal.CalculoEnvio();
+        EXPECT_FLOAT_EQ(0.135, actual);
+    }
+
+     TEST(ServicioPostalTests, CalculoEnvio_segundaClase_situacion3_Test)
+    {
+        ServicioPostal servicioPostal{11.0, 3.0, 2};
+        float actual = servicioPostal.CalculoEnvio();
+        EXPECT_FLOAT_EQ(0.150, actual);
+    }
+
+     TEST(ServicioPostalTests, CalculoEnvio_terceraClase_situacion1_Test)
+    {
+        ServicioPostal servicioPostal{2.0, 2.0, 3};
+        float actual = servicioPostal.CalculoEnvio();
+        EXPECT_FLOAT_EQ(0.030, actual);
+    }
+
+     TEST(ServicioPostalTests, CalculoEnvio_terceraClase_situacion2_Test)
+    {
+        ServicioPostal servicioPostal{4.0, 2.0, 3};
+        float actual = servicioPostal.CalculoEnvio();
+        EXPECT_FLOAT_EQ(0.032, actual);
+    }
+
+     TEST(ServicioPostalTests, CalculoEnvio_terceraClase_situacion3_Test)
+    {
+        ServicioPostal servicioPostal{10.0, 2.0, 3};
+        float actual = servicioPostal.CalculoEnvio();
+        EXPECT_FLOAT_EQ(0.034, actual);
     }
 }
